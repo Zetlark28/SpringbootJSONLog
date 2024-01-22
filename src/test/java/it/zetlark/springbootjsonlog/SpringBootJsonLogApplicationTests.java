1f27e8d9281c60d5b4e98e840fe2f2c73ce357e9
Zetlark28 <clark.ezpeleta@beije.it>
2024-01-22 09:53:21 +0100

    add properties and test class

---
+++ src/test/java/it/zetlark/springbootjsonlog/SpringBootJsonLogApplicationTests.java
@@ -0,0 +1,13 @@
+package it.zetlark.springbootjsonlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJsonLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
